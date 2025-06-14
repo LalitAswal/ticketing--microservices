57a2fc88daf8a258c7da84af7359d7f134dd2b11
LalitAswal <Lalit.Aswal143@gmail.com>
2025-06-14 22:07:31 +0530

    minor changes

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,6 +4,11 @@ import { json } from 'body-parser';
 const app = express();
 app.use(json())
 
-app.listen(()=>{
+
+app.get('/api/user/currentUser',(req, res)=>{
+    res.send('Hi there!!')
+})
+
+app.listen(3000,()=>{
     console.log(`port is Listen at 3000!!!!!!!`)
 })
